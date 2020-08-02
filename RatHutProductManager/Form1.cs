59053b0a1ebe158497d900c0596efce79ef881a9
SethSterling <63669378+SethSterling@users.noreply.github.com>
2020-08-01 17:50:15 -0700

    Issue#2 create layout of form (#13)
    * Added basic layout to initial from.
    
    * Updated the properties of the Form1 and objects on the form. Form1 is now RatHutProductManager.
    
    * Created methods for Add, Update, and Delete. Currently thorws NotImplementedException.
    
    * Renamed RatHutProductManager ro Form1
    
    * Created the Form1 layout.

--- RatHutProductManager/Form1.cs
+++ RatHutProductManager/Form1.cs
@@ -16,5 +16,29 @@ namespace RatHutProductManager
         {
             InitializeComponent();
         }
+
+        /// <summary>
+        /// Opens a form that allows you to add a product to the database.
+        /// </summary>
+        private void BtnAddProduct_Click(object sender, EventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Opens a form that allows you to modify a product from the database.
+        /// </summary>
+        private void BtnUpdateProduct_Click(object sender, EventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Open a prompt that allows you to delete a product from the database.
+        /// </summary>
+        private void BtnDeleteProduct_Click(object sender, EventArgs e)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
