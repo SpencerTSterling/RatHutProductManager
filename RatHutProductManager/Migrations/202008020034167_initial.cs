aea06577e9fdec7a1925b9b1467515f5f08455c5
Spencer Sterling <spencer.sterling2001@gmail.com>
2020-08-01 17:36:09 -0700

    Created the database

---
+++ RatHutProductManager/Migrations/202008020034167_initial.cs
@@ -0,0 +1,29 @@
+﻿namespace RatHutProductManager.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class initial : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Products",
+                c => new
+                    {
+                        ProductId = c.Int(nullable: false, identity: true),
+                        Title = c.String(),
+                        Price = c.Double(nullable: false),
+                        Description = c.String(),
+                        Category = c.String(),
+                    })
+                .PrimaryKey(t => t.ProductId);
+            
+        }
+        
+        public override void Down()
+        {
+            DropTable("dbo.Products");
+        }
+    }
+}
