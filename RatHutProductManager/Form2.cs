8f77cd5292fb6f390c7e3dba736f0675513179f2
Spencer Sterling <spencer.sterling2001@gmail.com>
2020-08-04 18:03:15 -0700

    Validation for all inputs on the Add form

--- RatHutProductManager/Form2.cs
+++ RatHutProductManager/Form2.cs
@@ -29,7 +29,34 @@ namespace RatHutProductManager
 
         private void BtnAddToDB_Click(object sender, EventArgs e)
         {
-            throw new NotImplementedException();
+            // Make sure all inputs on forms are valid 
+            #region Validation
+            if (!Validator.IsPresent(TxtProductTitle) ||
+                !Validator.IsPresent(TxtProductDesc))
+            {
+                MessageBox.Show("Please input a title and description for your product");
+            }
+
+            if ( !Validator.IsInt32(TxtProductPrice.Text))
+            {
+                MessageBox.Show("Please input a numerial value for the price");
+            }
+
+                // if none of the buttons are clicked, give error
+            if ( !RbCatBreakfast.Checked &&
+                 !RbCatBurgers.Checked &&
+                 !RbCatChickenSand.Checked &&
+                 !RbCatDessert.Checked &&
+                 !RbCatSides.Checked &&
+                 !RbCatMisc.Checked)
+            {
+                MessageBox.Show("Please choose a category for the product");
+            }
+            #endregion
+
+
+
+
         }
     }
 }
